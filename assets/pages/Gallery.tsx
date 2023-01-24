806ae9cf785607fb83a518ada1b5c5c7dbd8c24a
Andreas Greimel <andreas@mintgarden.io>
2023-01-24 17:37:22 +0100

    Fetch collection NFTs in the frontend and show as a basic grid

--- assets/pages/Gallery.tsx
+++ assets/pages/Gallery.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import useSWR from 'swr';
 import { Container } from '../components/Container';
+import { Nft } from '../interfaces/nft';
 
-export const Gallery: React.FC = () => (
-  <>
-    <Container className="mt-16 sm:mt-32">
-      <h1 className="text-zinc-800 dark:text-zinc-100">Gallery</h1>
-      <Link className="text-zinc-800 dark:text-zinc-100" to="/">
-        Go back{' '}
-      </Link>
-    </Container>
-  </>
-);
+// @ts-ignore
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export const Gallery: React.FC = () => {
+  let collectionId = 'col1vkehesfftd7j9ae7ufaq42rtry69hckm00tf70tc527jkq42qw6sk0pxpy';
+  const { data: collection, error, isLoading } = useSWR(`/api/collections/${collectionId}`, fetcher);
+
+  const {
+    data: collectionNfts,
+    error: errorNfts,
+    isLoading: isLoadingNfts,
+  } = useSWR(`/api/collections/${collectionId}/nfts`, fetcher);
+
+  const renderHeader = () => (isLoading ? <div>Loading...</div> : <div>{collection.name}</div>);
+  const renderGallery = () =>
+    isLoadingNfts ? (
+      <div>Loading NFTs...</div>
+    ) : (
+      <div className="grid grid-cols-3 gap-4">
+        {collectionNfts['hydra:member'].map((nft: Nft) => (
+          <div className="flex flex-col items-center rounded-xl border">
+            <div>
+              <img className="rounded-t-xl" src={nft.thumbnailUri} />
+            </div>
+            <div className="px-3 py-4 flex w-full">
+              <div className="text-lg font-bold text-zinc-800">{nft.name}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  return (
+    <>
+      <Container className="my-16 sm:mt-32">
+        {/*<div>{renderHeader()}</div>*/}
+        <div>{renderGallery()}</div>
+      </Container>
+    </>
+  );
+};
