acf260446682930d51576a77b6b3844e91927396
Andreas Greimel <andreas@mintgarden.io>
2023-01-27 12:31:21 +0100

    Add attributes and improve styling of NFT page

--- assets/components/Container.tsx
+++ assets/components/Container.tsx
@@ -1,42 +1,29 @@
-import React, { forwardRef } from 'react'
-import clsx from 'clsx'
+import clsx from 'clsx';
+import React, { forwardRef } from 'react';
 
-const OuterContainer = forwardRef(function OuterContainer(
-  { className, children, ...props }: any,
-  ref
-) {
+const OuterContainer = forwardRef(function OuterContainer({ className, children, ...props }: any, ref) {
   return (
     <div ref={ref} className={clsx('sm:px-8', className)} {...props}>
-      <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
+      <div className="w-full max-w-7xl lg:mx-auto lg:px-8">{children}</div>
     </div>
-  )
-})
+  );
+});
 
-const InnerContainer = forwardRef(function InnerContainer(
-  { className, children, ...props }: any,
-  ref
-) {
+const InnerContainer = forwardRef(function InnerContainer({ className, children, ...props }: any, ref) {
   return (
-    <div
-      ref={ref}
-      className={clsx('relative px-4 sm:px-8 lg:px-12', className)}
-      {...props}
-    >
+    <div ref={ref} className={clsx('relative px-4 sm:px-8 lg:px-12', className)} {...props}>
       <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
     </div>
-  )
-})
+  );
+});
 
-export const Container: any = forwardRef(function Container(
-  { children, ...props }: any,
-  ref
-) {
+export const Container: any = forwardRef(function Container({ children, ...props }: any, ref) {
   return (
     <OuterContainer ref={ref} {...props}>
       <InnerContainer>{children}</InnerContainer>
     </OuterContainer>
-  )
-})
+  );
+});
 
-Container.Outer = OuterContainer
-Container.Inner = InnerContainer
+Container.Outer = OuterContainer;
+Container.Inner = InnerContainer;
--- assets/pages/nfts/[id].tsx
+++ assets/pages/nfts/[id].tsx
@@ -16,39 +16,77 @@ export const NftPage: React.FC = () => {
 
     const {data: nft, error, isLoading} = useSWR<Nft>(`/api/nfts/${id}`, fetcher);
 
-    return (
-        <Container className="flex flex-col items-center">
-            <div className="mt-4 flex h-[50vh] w-full lg:h-[80vh]">
-                <div className="flex items-center justify-center">
-                    {nft && (
+    function renderAttributes(attributes: any[]) {
+        return (
+            <>
+                <h3 className="text-2xl font-bold">Attributes</h3>
+                <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
+                    {attributes.map((attribute) => (
                         <div
-                            className={
-                                isZoomedIn
-                                    ? 'fixed inset-0 z-50 flex h-auto max-h-full w-auto max-w-full items-center justify-center bg-zinc-800'
-                                    : 'flex-1 rounded-xl'
-                            }
+                            className="rounded-lg border border-zinc-200 dark:border-zinc-600"
+                            key={attribute.name || attribute.trait_type}
                         >
-                            <img
-                                onClick={toggleZoom}
-                                src={nft.previewUri}
-                                className={`max-h-full ${
-                                    isZoomedIn ? 'relative cursor-zoom-out' : 'cursor-zoom-in rounded-xl'
-                                } object-contain`}
-                            />
+                            <div className="relative block h-full rounded-lg p-4">
+                                <p className="break-words text-sm font-semibold text-zinc-500 dark:text-zinc-400">
+                                    {attribute.name || attribute.trait_type}
+                                </p>
+                                <p className="break-words text-base font-semibold text-zinc-900 dark:text-zinc-100">
+                                    {attribute.value || <>&nbsp;</>}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </>
+        );
+    }
+
+    // const nft: any = undefined;
+    return (
+        <Container>
+            <div className="my-8 flex flex-col gap-8">
+                <div className="mt-4 flex w-full lg:h-[80vh]">
+                    {nft ? (
+                        <div className="flex w-full items-center justify-center overflow-hidden">
+                            <div
+                                className={
+                                    (isZoomedIn
+                                        ? 'fixed inset-0 z-50 flex h-auto max-h-full w-auto max-w-full items-center justify-center bg-zinc-800'
+                                        : 'rounded-xl') + ' h-full'
+                                }
+                            >
+                                <img
+                                    onClick={toggleZoom}
+                                    src={nft.previewUri}
+                                    className={`max-h-full ${
+                                        isZoomedIn ? 'relative cursor-zoom-out' : 'cursor-zoom-in rounded-xl'
+                                    } object-contain`}
+                                />
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="h-full w-full rounded-xl">
+                            <div
+                                className="aspect-1 h-full w-full animate-pulse rounded-xl bg-zinc-100 dark:bg-zinc-800"></div>
                         </div>
                     )}
                 </div>
-            </div>
-            <div
-                className="flex rounded-xl bg-white/90 p-6 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-                {nft ? (
-                    <div>
-                        <h1 className="text-4xl font-semibold">{nft.name}</h1>
-                        <p className="mt-4">{nft.description}</p>
-                    </div>
-                ) : (
-                    <div>Loading...</div>
-                )}
+                <div
+                    className="flex rounded-xl bg-white/90 p-6 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+                    {nft ? (
+                        <div>
+                            <h1 className="text-5xl font-semibold">{nft.name}</h1>
+                            <p className="mt-4">{nft.description}</p>
+                            {nft.attributes && nft.attributes.length > 0 ? (
+                                <div className="mt-12">{renderAttributes(nft.attributes)}</div>
+                            ) : (
+                                ''
+                            )}
+                        </div>
+                    ) : (
+                        <div>Loading...</div>
+                    )}
+                </div>
             </div>
         </Container>
     );
