19bc8c80b97e775dcce85973cf933d7058959a66
Andreas Greimel <andreas@mintgarden.io>
2023-01-27 10:56:34 +0100

    Add collection grid view to Gallery page

--- assets/pages/Gallery.tsx
+++ assets/pages/Gallery.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useSWR from 'swr';
 import { Container } from '../components/Container';
 import { Nft } from '../interfaces/nft';
-
-// @ts-ignore
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+import {fetcher} from "../utilities/fetcher";
 
 export const Gallery: React.FC = () => {
   let collectionId = 'col1vkehesfftd7j9ae7ufaq42rtry69hckm00tf70tc527jkq42qw6sk0pxpy';
@@ -21,16 +20,19 @@ export const Gallery: React.FC = () => {
     isLoadingNfts ? (
       <div>Loading NFTs...</div>
     ) : (
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-x-6 gap-y-8">
         {collectionNfts['hydra:member'].map((nft: Nft) => (
-          <div className="flex flex-col items-center rounded-xl border">
+          <Link
+            to={`/nfts/${nft.id}`}
+            className="flex flex-col items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+          >
             <div>
               <img className="rounded-t-xl" src={nft.thumbnailUri} />
             </div>
-            <div className="px-3 py-4 flex w-full">
-              <div className="text-lg font-bold text-zinc-800">{nft.name}</div>
+            <div className="flex w-full px-3 py-4">
+              <div className="text-lg font-bold">{nft.name}</div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     );
