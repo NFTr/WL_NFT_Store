b1f5904e9f38a11e3f460330e2e647fc5892faec
Andreas Greimel <andreas@mintgarden.io>
2023-01-27 10:56:18 +0100

    Improve homepage styling

--- assets/App.tsx
+++ assets/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import {Outlet, useLocation} from 'react-router-dom';
 import { Header } from './components/Header';
 
-export const App: React.FC = () => (
-  <>
-    <div className="fixed inset-0 flex justify-center sm:px-8">
-      <div className="flex w-full max-w-7xl lg:px-8">
-        <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-      </div>
-    </div>
-    <div className="relative">
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      {/*<Footer />*/}
-    </div>
-  </>
-);
+export const App: React.FC = () => {
+    let isHomePage = useLocation().pathname === '/';
+
+    return (
+        <>
+            <div className="fixed inset-0 flex justify-center sm:px-8">
+                <div className="flex w-full max-w-7xl lg:px-8">
+                    <div className={isHomePage ? "w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" : 'w-full'}/>
+                </div>
+            </div>
+            <div className="relative">
+                <Header/>
+                <main>
+                    <Outlet/>
+                </main>
+                {/*<Footer />*/}
+            </div>
+        </>
+    );
+};
