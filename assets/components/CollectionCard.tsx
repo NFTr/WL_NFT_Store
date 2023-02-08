c85fc36edacb7f015b5fe6a34f65332c38e33401
jonathanDeul <jona@familie-deul.de>
2023-02-08 11:54:00 +0100

    adding page and adapted list for all collections

---
+++ assets/components/CollectionCard.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Collection } from '../interfaces/collection';
+
+export const CollectionCard: React.FC<{ collection: Collection, gridStyle: string }> = ({ collection, gridStyle }) => {
+    const bannerUrl = collection?.attributes.find((attributes: { type: string; }) => attributes.type === "banner").value;
+    const iconUrl = collection?.attributes.find((attributes: { type: string; }) => attributes.type === "icon").value;
+    const description = collection?.attributes.find((attributes: { type: string; }) => attributes.type === "description").value;
+
+    if (gridStyle === "grid-compact") {
+        return <Link
+        to={`/collection/${collection.id}`}
+        className="flex flex-col items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+    >
+        <div>
+            <img className="rounded-t-xl" src={iconUrl} />
+        </div>
+        <div className="flex w-full px-3 py-4">
+            <div className="text-lg font-bold">{collection.name}</div>
+        </div>
+    </Link>;
+    } else if (gridStyle === "list") {
+        return <Link
+        to={`/collection/${collection.id}`}
+        className="rounded-xl w-full h-32 bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+        
+    >
+        <div className="w-full h-32 flex bg-center blur-sm brightness-110 dark:brightness-50" style={{ backgroundImage: `url(${bannerUrl})` }}></div>
+        <div className="w-full h-32 flex items-center -translate-y-32"> 
+            <div className="w-28 sm:w-48 mx-4">
+                <img className="rounded-xl" src={iconUrl} />
+            </div>
+            
+            <div className="text-xl font-bold">{collection.name}</div>
+            <div className="ml-4 text-xs leading-none md:text-base xl:text-lg text-block hidden sm:flex">{description}</div>
+
+        </div>
+    </Link>;
+    } else if (gridStyle === "grid") {
+        return <Link
+        to={`/collection/${collection.id}`}
+        className="flex flex-col items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+    >
+        <div>
+            <img className="rounded-t-xl" src={iconUrl} />
+        </div>
+        <div className="flex w-full px-3 py-4">
+            <div className="text-lg font-bold">{collection.name}</div>
+        </div>
+    </Link>;
+    } else  {
+        return <Link
+        to={`/collection/${collection.id}`}
+        className="flex flex-col items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+    >
+        <div>
+            <img className="rounded-t-xl" src={iconUrl} />
+        </div>
+        <div className="flex w-full px-3 py-4">
+            <div className="text-lg font-bold">{collection.name}</div>
+        </div>
+    </Link>;
+    }
+};
---
+++ assets/components/CollectionList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Collection } from "../interfaces/Collection";
+import { CollectionCard } from "./CollectionCard";
+
+export const CollectionList: React.FC<{ collections: any, gridStyle: string }> = ({ collections, gridStyle }) => {
+    if (gridStyle === "grid-compact") {
+    return <div className="grid grid-cols-2 gap-x-6 gap-y-8 lg:grid-cols-3">
+            {collections['hydra:member'].map((collection: Collection) => (
+            <CollectionCard key={collection.id} collection={collection} gridStyle={gridStyle} />
+            ))}
+        </div>
+    } else if (gridStyle === "list") {
+        return <div className="grid grid-cols-1 gap-x-6 gap-y-8">
+            {collections['hydra:member'].map((collection: Collection) => (
+           <CollectionCard key={collection.id} collection={collection} gridStyle={gridStyle} />
+            ))} 
+        </div>
+    } else {
+        return <div className="grid grid-cols-4 gap-x-6 gap-y-8 sm:grid-cols-4 lg:grid-cols-6">
+            {collections['hydra:member'].map((collection: Collection) => (
+            <CollectionCard key={collection.id} collection={collection} gridStyle={gridStyle} />
+            ))}
+        </div>
+    }
+};
