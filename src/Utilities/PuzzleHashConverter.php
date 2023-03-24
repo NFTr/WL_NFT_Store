ee0e9431828098c9726e22aeeffc04c40a1a5042
Andreas Greimel <andreas@mintgarden.io>
2023-03-24 09:50:44 +0100

    Fix puzzle hash encoding
    The hex string was not read into the encoding correctly, so it outputted a wrong encoded string

--- src/Utilities/PuzzleHashConverter.php
+++ src/Utilities/PuzzleHashConverter.php
@@ -6,11 +6,12 @@ use function BrooksYang\Bech32m\convertBits;
 use function BrooksYang\Bech32m\encode;
 use const BrooksYang\Bech32m\BECH32M;
 
-class PuzzleHashConverter {
+class PuzzleHashConverter
+{
 
     public function encodePuzzleHash(string $puzzleHash, string $hrp = 'xch'): string
     {
         $dataArray = unpack('C*', hex2bin(str_replace('0x', '', $puzzleHash)));
-        return encode($hrp, convertBits(array_slice($dataArray, 1), count($dataArray) - 1, 8, 5), BECH32M);
+        return encode($hrp, convertBits(array_values($dataArray), count($dataArray), 8, 5), BECH32M);
     }
 }
