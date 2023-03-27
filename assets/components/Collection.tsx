14a691cf04f73cf998fe12537aed2bb3f651422d
jonathanDeul <jona@familie-deul.de>
2023-03-27 09:18:57 +0200

    cleanup NftCard & add offer

--- assets/components/Collection.tsx
+++ assets/components/Collection.tsx
@@ -3,22 +3,12 @@ import { Link } from 'react-router-dom';
 import { Nft } from '../interfaces/nft';
 import { NftCard } from './NftCard';
 
-export const Collection: React.FC<{ collectionNfts: any; gridStyle: String }> = ({ collectionNfts, gridStyle }) => {
+export const Collection: React.FC<{ collectionNfts: any; gridStyle: string }> = ({ collectionNfts, gridStyle }) => {
   if (gridStyle === 'list') {
     return (
       <div className="grid grid-cols-1 gap-x-6 gap-y-8">
         {collectionNfts['hydra:member'].map((nft: Nft) => (
-          <Link
-            to={`/nfts/${nft.id}`}
-            className="h-42 grid grid-cols-5 items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
-          >
-            <div>
-              <img className="m-2 h-40 w-40 rounded-xl object-cover" src={nft.thumbnailUri} />
-            </div>
-            <div className="flex w-full px-3 py-4">
-              <div className="text-lg font-bold">{nft.name}</div>
-            </div>
-          </Link>
+          <NftCard key={nft.id} nft={nft} gridStyle={gridStyle} />
         ))}
       </div>
     );
@@ -28,7 +18,7 @@ export const Collection: React.FC<{ collectionNfts: any; gridStyle: String }> = 
     return (
       <div className={`grid ${gridClasses} gap-x-6 gap-y-8`}>
         {collectionNfts['hydra:member'].map((nft: Nft) => (
-          <NftCard key={nft.id} nft={nft} />
+          <NftCard key={nft.id} nft={nft} gridStyle={gridStyle} />
         ))}
       </div>
     );
--- assets/components/NftCard.tsx
+++ assets/components/NftCard.tsx
@@ -1,19 +1,77 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nft } from '../interfaces/nft';
+import useSWR from 'swr';
+import { fetcher } from '../utilities/fetcher';
 
-export const NftCard: React.FC<{ nft: Nft }> = ({ nft }) => {
-  return (
-    <Link
-      to={`/nfts/${nft.id}`}
-      className="flex flex-col items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
-    >
-      <div>
-        <img className="rounded-t-xl" src={nft.thumbnailUri} />
-      </div>
-      <div className="flex w-full px-3 py-4">
-        <div className="text-lg font-bold">{nft.name}</div>
-      </div>
-    </Link>
-  );
+export const NftCard: React.FC<{ nft: Nft; gridStyle: string }> = ({ nft, gridStyle }) => {
+  const { data: offers, error: offerError, isLoading: isLoadingOffer } = useSWR(`/api/nfts/${nft.id}/offers`, fetcher);
+  if (gridStyle == 'list') {
+    return (
+      <Link
+        to={`/nfts/${nft.id}`}
+        className="h-42 flex items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+      >
+        <div>
+          <img className="m-2 h-40 w-40 rounded-xl object-cover" src={nft.thumbnailUri} />
+        </div>
+        <div className="px-5">
+          <div className="text-lg font-bold">{nft.name}</div>
+        </div>
+        <div className="">
+          {offers ? (
+            offers['hydra:member'].map((offer: any) => (
+              <div className="flex items-center">
+                <img className="h-8" src="/chiaLogo.png" alt="Chia logo" />
+                <div>{offer.requested[0].amount} XCH</div>
+              </div>
+            ))
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </Link>
+    );
+  } else if (gridStyle == 'grid') {
+    return (
+      <Link
+        to={`/nfts/${nft.id}`}
+        className="flex flex-col items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+      >
+        <div>
+          <div></div>
+          <img className="h-40 w-40 rounded-xl object-cover" src={nft.thumbnailUri} />
+          <div className="absolute bottom-0 w-full rounded-b-xl bg-white/70 transition-all duration-200 hover:bg-white/90 hover:py-2 hover:text-xl">
+            <div className="text-center text-xs font-bold">{nft.name}</div>
+          </div>
+        </div>
+      </Link>
+    );
+  } else {
+    return (
+      <Link
+        to={`/nfts/${nft.id}`}
+        className="flex flex-col items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+      >
+        <div>
+          <img className="rounded-t-xl" src={nft.thumbnailUri} />
+        </div>
+        <div className="flex w-full justify-between px-3 py-4">
+          <div className="text-lg font-bold">{nft.name}</div>
+          <div className="">
+            {offers ? (
+              offers['hydra:member'].map((offer: any) => (
+                <div className="flex items-center">
+                  <img className="h-8" src="/chiaLogo.png" alt="Chia logo" />
+                  <div>{offer.requested[0].amount} XCH</div>
+                </div>
+              ))
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      </Link>
+    );
+  }
 };
