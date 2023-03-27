729fa62f07041da94b49311c78d6f3fcfb00a860
Andreas Greimel <andreas@mintgarden.io>
2023-03-27 11:47:15 +0200

    Fix casing for svg attributes

--- assets/pages/Gallery.tsx
+++ assets/pages/Gallery.tsx
@@ -59,8 +59,8 @@ export const Grid_K = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-black dark:fill-white">
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
       />
     </svg>
@@ -90,7 +90,7 @@ export const List = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-black dark:fill-white" viewBox="0 0 16 16">
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
       />
     </svg>
