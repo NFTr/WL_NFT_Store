8827dd70cb3fcab310d0047429c4eb8c3bf1c95c
Andreas Greimel <andreas@mintgarden.io>
2023-05-10 12:46:47 +0200

    Use square aspect ratio for NFT cards to reduce layout shifting

--- assets/components/NftCard.tsx
+++ assets/components/NftCard.tsx
@@ -23,7 +23,7 @@ const NftSkeleton: React.FC<{ gridStyle: string }> = ({ gridStyle }) => {
   } else {
     return (
       <div className="flex animate-pulse flex-col items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <div className="h-40 w-full rounded-t-xl bg-gray-300"></div>
+        <div className="aspect-1 w-full rounded-t-xl bg-gray-300"></div>
         <div className="flex w-full justify-between px-3 py-4">
           <div className="h-4 w-40 rounded bg-gray-300"></div>
           <div className="h-8 w-32 rounded bg-gray-300"></div>
@@ -45,7 +45,7 @@ export const NftCard: React.FC<{ nft?: Nft; gridStyle: string }> = ({ nft, gridS
         className="h-42 flex items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
       >
         <div>
-          <img className="m-2 h-40 w-40 rounded-xl object-cover" src={nft.thumbnailUri} />
+          <img className="m-2 h-40 w-40 rounded-xl object-cover aspect-1" src={nft.thumbnailUri} />
         </div>
         <div className="px-5">
           <div className="text-lg font-bold">{nft.name}</div>
@@ -84,7 +84,7 @@ export const NftCard: React.FC<{ nft?: Nft; gridStyle: string }> = ({ nft, gridS
         className="flex flex-col items-center rounded-xl bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5  backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
       >
         <div>
-          <img className="rounded-t-xl" src={nft.thumbnailUri} />
+          <img className="aspect-1 object-cover rounded-t-xl" src={nft.thumbnailUri} />
         </div>
         <div className="flex w-full justify-between px-3 py-4">
           <div className="text-lg font-bold">{nft.name}</div>
