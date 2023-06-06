25186b36626506368c13accd608547b21ca0f75b
Andreas Greimel <andreas@mintgarden.io>
2023-06-06 21:35:30 +0200

    Add ScrollRestoration component to restore scroll position after navigation

--- assets/App.tsx
+++ assets/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Outlet, useLocation} from 'react-router-dom';
+import {Outlet, ScrollRestoration, useLocation} from 'react-router-dom';
 import {Header} from './components/Header';
 
 export const App: React.FC = () => {
@@ -20,6 +20,7 @@ export const App: React.FC = () => {
                 </main>
                 {/*<Footer />*/}
             </div>
+            <ScrollRestoration/>
         </>
     );
 };
